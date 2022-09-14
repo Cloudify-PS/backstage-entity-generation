d9eb089f10e18d6965a3d67d35f9fd2bfa38eccd
Michał Mordawski <michal.mordawski@amartus.com>
2022-09-14 10:35:30 +0200

    microservice entity

--- scripts/refresh_blueprints.py
+++ scripts/refresh_blueprints.py
@@ -33,6 +33,30 @@ spec:
   owner: team-a
 '''
 
+entity_template_ms = '''
+apiVersion: backstage.io/v1alpha1
+kind: Component
+metadata:
+  name: {blueprint_id}
+  description: {description}
+  annotations:
+    backstage.io/kubernetes-id: {blueprint_id}
+  tags:
+    - tosca
+    - cloudify
+  links:
+    - url: http://{manager_ip}/console/page/blueprints_blueprint/{blueprint_id}?c=%5B%7B%22context%22%3A%7B%7D%7D%2C%7B%22context%22%3A%7B%22blueprintId%22%3A%22{blueprint_id}%22%7D%2C%22pageName%22%3A%22{blueprint_id}%22%7D%5D#!
+      title: Deploy component
+      icon: user
+    - url: http://{repo_ip}/projects/CLOUD/repos/nodejs-test/browse
+      title: {repo_name} repo
+      icon: docs
+spec:
+  type: service
+  lifecycle: production
+  owner: team-a
+'''
+
 def _get_client():
     return manager.get_rest_client()
 
@@ -77,6 +101,21 @@ def create_entity_file(blueprint, manager_host, repo_path, **kwargs):
         file.write(entity_definition)
     add_file_change(blueprint_entity_file_name, repo_path)
 
+def ms1_v1_entity_file(repo_path, **kwargs):
+    template_params= {
+        "blueprint_id": "ms1-v1",
+        "manager_ip": "44.194.70.211",
+        "repo_ip": "44.194.70.211:7990",
+        "description": "Microservice 1 blueprint",
+        "repo_name": "MS1"
+    }
+    entity_definition = entity_template_ms.format(**template_params)
+    blueprint_entity_file_name = 'ms1-v1.yaml'
+    entity_file_path = os.path.join(repo_path, blueprint_entity_file_name)
+    with open(entity_file_path, "w") as file:
+        file.write(entity_definition)
+    add_file_change(blueprint_entity_file_name, repo_path)
+
 
 client = _get_client()
 
@@ -86,5 +125,7 @@ if inputs['clean_repo']:
 for blueprint in client.blueprints.list():
     if blueprint.id != ctx.blueprint.id:
         create_entity_file(blueprint, **credentials)
+
+ms1_v1_entity_file(**credentials)
 
 push_changes(**credentials)
